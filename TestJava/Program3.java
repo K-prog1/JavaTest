e91a20bd6f56b09a92adbbee63bb36a47737a798
K-prog1 <gercovdaniil21@gmail.com>
2026-08-09 22:08:40 +1000

    FixedSomeProblems

--- TestJava/Program3.java
+++ TestJava/Program3.java
@@ -1,21 +1,41 @@
+import java.util.Arrays;
+
 public class Program3 {
     public static void main(String[] args){
-
-        int randomint = (int) (Math.random() * 101);
+       
         int randlist[] = new int[5]; 
+        int temp;
+
+
+        for (int i = 0; i < 5; i++){
+            randlist[i] = (int) (Math.random() * 101);
+        }
+        
+        int sortedlist[] = Arrays.copyOf(randlist, randlist.length);
         int min = randlist[0];
         int max = randlist[0];
+
         
         for (int i = 0; i < 5;i++ ){
-            randlist[i] = randomint;
-            min = randlist[0];
-            max = randlist[0];
             if (randlist[i] < min) min = randlist[i];
             if (randlist[i]> max) max = randlist[i];
 
         }
-           System.out.print(randlist);
-           System.out.print(min);
-           System.out.print(max);
+        for (int j = 0; j < sortedlist.length; j++){
+            for (int i = 0; i < sortedlist.length - j - 1; i++){
+                if (sortedlist[i] > sortedlist[i+1]){
+                    temp = sortedlist[i];
+                    sortedlist[i] = sortedlist[i + 1];
+                    sortedlist[i+1] = temp;
+
+            }
+        }
+
+        }
+        
+           System.out.println("Сортированный Массив:" + Arrays.toString(sortedlist));
+           System.out.println("Массив: " + Arrays.toString(randlist));
+           System.out.println("Минимум: " + min);
+           System.out.println("Максимум: " + max);
     }
 }
